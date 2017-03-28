1c364fa806192917d6cc0dfd1b5f7dbf1018f99b
Noah Reid <noah.reid@gmail.com>
2017-03-28 11:16:28 -0400

    blah

--- grandis_align.sh
+++ grandis_align.sh
@@ -4,6 +4,8 @@
 #2 fastq2
 #3 ID table
 
+# need to load parallel and samtools
+
 
 sam=$(echo $1 | grep -oP "BU[0-9]+")
 pop=$(cat $3 | grep $sam | cut -f 3)
@@ -13,17 +15,19 @@ fq2=$2
 
 rg=$(echo \@RG\\tID:$sam.combined\\tPL:Illumina\\tPU:x\\tLB:combined\\tSM:$sam.$pop)
 
+outdir=/scratch/nmr15102/grandis_align
+outroot=$sam\_$pop
 
-# module load 
 
-# bwagenind=/home/nreid/popgen/kfish3/killifish20130322asm.fa
 
-# BWA=~/bin/bwa/bwa
-# SBL=~/bin/samblaster/samblaster
+bwagenind=/scratch/nmr15102/fhet_genome/GCF_000826765.1_Fundulus_heteroclitus-3.0.2_genomic.fna.gz
 
-# cmdline=$BWA\ mem\ $bwagenind\ -t\ 24\ -R\ $rg\ $fq1\ $fq2
-# echo $cmdline
+BWA=~/bin/bwa/bwa
+SBL=~/bin/samblaster/samblaster
 
-# #execute bwa command line, pipe to samblaster to mark duplicates and create files containing discordant and split alignments, then to samtools to sort output. 
-# $cmdline | $SBL -e -d $bwadir/$outroot.disc.sam -s $bwadir/$outroot.split.sam | samtools view -S -h -u - | samtools sort - $bwadir/$outroot
+cmdline=$BWA\ mem\ $bwagenind\ -t\ 2\ -R\ $rg\ $fq1\ $fq2
+echo $cmdline
+
+# execute bwa command line, pipe to samblaster to mark duplicates and create files containing discordant and split alignments, then to samtools to sort output. 
+$cmdline | $SBL -e -d $outdir/$outroot.disc.sam -s $outdir/$outroot.split.sam | samtools view -S -h -u - | samtools sort -O BAM -T $outdir/$outroot -o $outdir/$outroot.bam - 
 
