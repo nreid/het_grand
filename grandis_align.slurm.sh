5dad7e7842848e67a7abf56b9b40477115981470
Noah Reid <noah.reid@gmail.com>
2017-03-28 16:45:16 -0400

    blah

--- grandis_align.slurm.sh
+++ grandis_align.slurm.sh
@@ -6,4 +6,4 @@ module load samtools
 
 source ~/bin/parallel-slurm/parallel-slurm-setup.sh
 
-parallel sh grandis_align.sh ::: $(ls /scratch/nmr15102/gradis_seq/combined1P*) :::+  $(ls /scratch/nmr15102/gradis_seq/combined2P*) ::: /home/nmr15102/het_grand/sexes.txt
+parallel sh ~/het_grand/grandis_align.sh ::: $(ls /scratch/nmr15102/gradis_seq/combined1P*) :::+  $(ls /scratch/nmr15102/gradis_seq/combined2P*) ::: /home/nmr15102/het_grand/sexes.txt
