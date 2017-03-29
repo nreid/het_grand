f0ac14f7dc152ac24d3a458f9f51034d4f7111b3
Noah Reid <noah.reid@gmail.com>
2017-03-29 10:38:10 -0400

    blah

--- grandis_align.slurm.sh
+++ grandis_align.slurm.sh
@@ -2,6 +2,9 @@
 #SBATCH --ntasks 12
 #SBATCH --cpus-per-task 1 
 
+# "module" alone was an unrecognized command. very confusing, the parallel slurm script runs this before "module" though. so I'm trying it. 
+source /etc/profile.d/modules.sh
+
 module load samtools 
 
 source ~/bin/parallel-slurm/parallel-slurm-setup.sh
