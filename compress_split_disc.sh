1959f9a6d8ad35273e75ccbc9b3cbe7cd6c0f833
Noah Reid <noah.reid@gmail.com>
2017-03-30 09:45:26 -0400

    blah

---
+++ compress_split_disc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#1 fastq1
+#2 fastq2
+#3 ID table
+
+# need to load parallel and samtools
+
+outfile=$(echo $1 | sed 's/sam//')
+
+# execute bwa command line, pipe to samblaster to mark duplicates and create files containing discordant and split alignments, then to samtools to sort output. 
+samtools view -S -h -u $1 | samtools sort -T $outfile - >$outfile.bam
+
+samtools index $outfile.bam
---
+++ grandis_disc_compress.slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks 20
+#SBATCH --cpus-per-task 1
+
+# "module" alone was an unrecognized command. very confusing, the parallel slurm script runs this before "module" though. so I'm trying it. 
+source /etc/profile.d/modules.sh
+
+module load samtools 
+
+source ~/bin/parallel-slurm/parallel-slurm-setup.sh
+
+$parallel sh ~/het_grand/compress_split_disc.sh ::: $(ls /scratch/nmr15102/grandis_align/*disc.sam)
+
