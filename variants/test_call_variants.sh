5fafe20b96817dfdaf3fb8216a2db10334ede3b3
Noah Reid <noah.reid@gmail.com>
2017-04-03 14:05:50 -0400

    blah

---
+++ variants/test_call_variants.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 4
+#SBATCH -N 1-1
+
+sh ~/het_grand/variants/call_variants_subregion.sh <(head -n /scratch/nmr15102/fhet_genome/chunks/gensplit_300.00001.bed)
