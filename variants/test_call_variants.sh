3dd14ed2350dc3e0ec935b287becddb517c469ce
Noah Reid <noah.reid@gmail.com>
2017-04-03 15:17:02 -0400

    blah

--- variants/test_call_variants.sh
+++ variants/test_call_variants.sh
@@ -3,4 +3,12 @@
 #SBATCH --cpus-per-task 4
 #SBATCH -N 1-1
 
-parallel -a /scratch/nmr15102/fhet_genome/chunks/gensplit_300.00001.bed sh ~/het_grand/variants/call_variants_subregion.sh $IN
+
+# "module" alone was an unrecognized command. very confusing, the parallel slurm script runs this before "module" though. so I'm trying it. 
+source /etc/profile.d/modules.sh
+
+module load samtools 
+
+source ~/bin/parallel-slurm/parallel-slurm-setup.sh
+
+$parallel -a /scratch/nmr15102/fhet_genome/chunks/gensplit_300.00001.bed sh ~/het_grand/variants/call_variants_subregion.sh $IN
