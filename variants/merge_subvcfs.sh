1f64af807bb1628cc64c50d9be6ee7b2b9b2c31a
Noah Reid <noah.reid@gmail.com>
2017-04-05 13:00:57 -0400

    blah

--- variants/merge_subvcfs.sh
+++ variants/merge_subvcfs.sh
@@ -11,5 +11,5 @@ START=$(sed -n "$num"p $WIN | awk '$2=$2+1' | cut -f 2 -d ' ')
 END=$(sed -n "$num"p $WIN | awk '$2=$2+1' | cut -f 3 -d ' ')
 GZ=$(sed -n "$num"p $WIN | awk '$2=$2+1' | sed 's/ /_/g')
 GZ=$GZ.vcf.gz
-zcat $SVDIR/$GZ | grep -v ^# | awk -v st="$START" -v en="$END" '$2>=st && $2<=en'
+zcat $SVDIR/$GZ | grep -v ^# | awk -v st="$START" -v en="$END" '$2>=st && $2<=en' | awk '$6>30'
 done 
