349ff78b12e6f8510e3d05ab4c026b054eff1553
Noah Reid <noah.reid@gmail.com>
2017-04-05 12:57:51 -0400

    blah

--- variants/merge_subvcfs.sh
+++ variants/merge_subvcfs.sh
@@ -3,7 +3,7 @@
 WIN=/scratch/nmr15102/fhet_genome/100kb_win.bed
 SVDIR=/scratch/nmr15102/variants/subvars/
 
-zcat NW_012234324.1_1_100000.vcf.gz | grep ^#
+zcat $SVDIR/NW_012234324.1_1_100000.vcf.gz | grep ^#
 
 for num in $(seq 1 18873)
 do
