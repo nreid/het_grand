163928b22bb9d4d7282dcf4a44788607f7926ae6
Noah Reid <noah.reid@gmail.com>
2017-04-05 12:57:10 -0400

    blah

--- variants/merge_subvcfs.sh
+++ variants/merge_subvcfs.sh
@@ -7,7 +7,9 @@ zcat NW_012234324.1_1_100000.vcf.gz | grep ^#
 
 for num in $(seq 1 18873)
 do
+START=$(sed -n "$num"p $WIN | awk '$2=$2+1' | cut -f 2 -d ' ')
+END=$(sed -n "$num"p $WIN | awk '$2=$2+1' | cut -f 3 -d ' ')
 GZ=$(sed -n "$num"p $WIN | awk '$2=$2+1' | sed 's/ /_/g')
 GZ=$GZ.vcf.gz
-zcat $SVDIR/$GZ | grep -v ^#
+zcat $SVDIR/$GZ | grep -v ^# | awk -v st="$START" -v en="$END" '$2>=st && $2<=en'
 done 
