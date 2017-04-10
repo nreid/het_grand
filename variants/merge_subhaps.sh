33a9d0ad47daf624b96715e6fe89a14ca974c6f5
Noah Reid <noah.reid@gmail.com>
2017-04-10 09:22:09 -0400

    blah

--- variants/merge_subhaps.sh
+++ variants/merge_subhaps.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 WIN=/scratch/nmr15102/fhet_genome/100kb_win.bed
-SVDIR=/scratch/nmr15102/variants/subvars/
+SVDIR=/scratch/nmr15102/variants/subhap/
 
-# this line pulls out a VCF header.
-zcat $SVDIR/NW_012234324.1_1_100000.vcf.gz | grep ^#
+# this line pulls out a VCF header. 
+zcat /scratch/nmr15102/variants/hetgrand.vcf.gz | head -n 12000 | grep ^#
 
 for num in $(seq 1 18873)
 do
 START=$(sed -n "$num"p $WIN | awk '$2=$2+1' | cut -f 2 -d ' ')
 END=$(sed -n "$num"p $WIN | awk '$2=$2+1' | cut -f 3 -d ' ')
 GZ=$(sed -n "$num"p $WIN | awk '$2=$2+1' | sed 's/ /_/g')
-GZ=$GZ.vcf.gz
-zcat $SVDIR/$GZ | grep -v ^# | awk -v st="$START" -v en="$END" '$2>=st && $2<=en' | awk '$6>30'
+GZ=$GZ.hap.vcf.gz
+zcat $SVDIR/$GZ | grep -v ^# 
 done 
