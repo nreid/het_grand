61836e5ad9c331ac1431516e1cb2496a61555b73
Noah Reid <noah.reid@gmail.com>
2017-04-10 21:11:31 -0400

    blah

---
+++ sweepfinder/write_sweepfinder_scaffold.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#module load r/3.2.3
+# NW_012224401.1
+
+TAB=~/bin/htslib/tabix
+
+HAPVCF=/scratch/nmr15102/variants/hetgrand.hap.vcf.gz
+
+$TAB $HAPVCF $1 | Rscript ~/het_grand/sweepfinder/write_sweepfinder_stream.R
