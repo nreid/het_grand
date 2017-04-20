079df11f515ccfa10e7ded10dc301762aa952002
Noah Reid <noah.reid@gmail.com>
2017-04-20 15:27:17 -0400

    blah

--- trees_fst/fst_allsites.sh
+++ trees_fst/fst_allsites.sh
@@ -3,7 +3,7 @@
 # "module" alone was an unrecognized command. very confusing, the parallel slurm script runs this before "module" though. so I'm trying it. 
 source /etc/profile.d/modules.sh
 
-module load r/3.2.3 
+#module load r/3.2.3 
 
 
 TAB=~/bin/htslib/tabix
@@ -11,5 +11,4 @@ BGZ=~/bin/htslib/bgzip
 
 zcat /scratch/nmr15102/variants/hetgrand.hap.vcf.gz | \
 grep -v ^# |  \
-Rscript ~/het_grand/trees_fst/fst_stream_bysite.R | \
-$BGZ -c >het_grand_fst.gz
+Rscript ~/het_grand/trees_fst/fst_stream_bysite.R | $BGZ -c | cat >het_grand_fst.gz
