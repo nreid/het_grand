a011e084ce6dc227bd07f0beae574d4294b91a3d
Noah Reid <noah.reid@gmail.com>
2017-04-24 13:26:36 -0400

    blah

--- trees_fst/fst_dxy_bysite.sh
+++ trees_fst/fst_dxy_bysite.sh
@@ -6,7 +6,7 @@ SCRIPT=~/het_grand/trees_fst/fst_dxy_bysite.R
 TAB=~/bin/htslib/tabix
 BGZ=~/bin/htslib/bgzip
 VCF=/scratch/nmr15102/variants/hetgrand.hap.vcf.gz
-OUTDIR=/scratch/nmr15102/variants
+OUTDIR=/scratch/nmr15102/variants/fst_scaffolds
 OUTFILE=$1.fst.gz
 
 $TAB $VCF $1 | Rscript $SCRIPT | $BGZ -c | cat >$OUTDIR/$OUTFILE
