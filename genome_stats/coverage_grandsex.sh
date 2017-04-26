5864bd7205634a78757eeccf69edbc9d29f2f6f3
Noah Reid <noah.reid@gmail.com>
2017-04-26 15:41:29 -0400

    blah

---
+++ genome_stats/coverage_grandsex.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# "module" alone was an unrecognized command. very confusing, the parallel slurm script runs this before "module" though. so I'm trying it. 
+source /etc/profile.d/modules.sh
+
+module load samtools/1.3
+
+
+LISTFILE=$1
+OUTFILE=$(echo $1 | sed 's/.*\///' | sed 's/list/coverage.gz/' )
+
+BGZ=~/bin/htslib/bgzip
+BAM=~/bin/bamtools/bin/bamtools
+BED=~/bin/bedtools2/bin/bedtools
+OUTDIR=/scratch/nmr15102/genome_stats/evalsites
+HICOV=/scratch/nmr15102/variants/meta/hicov.merge.sort.bed
+
+
+$BAM merge -list $1 | \
+$BAM filter -mapQuality ">30" -isProperPair true | \
+samtools depth -d 20000 -a /dev/stdin | \
+~/bin/htslib/bgzip -c >$OUTDIR/$OUTFILE
