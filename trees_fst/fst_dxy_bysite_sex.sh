f6c62b3136fe3bf67478cbaabd2f4624d6f0ca31
Noah Reid <noah.reid@gmail.com>
2017-04-26 21:11:15 -0400

    blah

---
+++ trees_fst/fst_dxy_bysite_sex.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+SCRIPT=~/het_grand/trees_fst/fst_dxy_bysite_sex.R
+
+TAB=~/bin/htslib/tabix
+BGZ=~/bin/htslib/bgzip
+VCF=/scratch/nmr15102/variants/hetgrand.hap.vcf.gz
+OUTDIR=/scratch/nmr15102/variants/fst_scaffolds_sex
+OUTFILE=$1.fst.gz
+
+$TAB $VCF $1 | Rscript $SCRIPT | $BGZ -c | cat >$OUTDIR/$OUTFILE
