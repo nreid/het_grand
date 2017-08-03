19ff0fce4265eed0d595e7a31e272a40a1ccb910
Noah Reid <noah.reid@gmail.com>
2017-08-03 09:39:17 -0400

    blah

---
+++ rna_align/mergebams.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+basename=$1
+
+inbam=/scratch/nmr15102/tolerance_rnaseq/rawdata
+outdir=/scratch/nmr15102/tolerance_rnaseq/alignments
+
+BAM=/home/nmr15102/bin/bamtools/bin/bamtools
+
+find $inbam -name "*$basename*bam" >$basename.list
+
+$BAM merge -list $basename.list -out $outdir/$basename.bam
+
+rm $basename.list
