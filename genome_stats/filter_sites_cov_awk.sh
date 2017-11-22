751c66afefa26251d37ff6b4279a6d721e17410d
Noah Reid <noah.reid@gmail.com>
2017-11-22 15:40:42 -0500

    coverage

--- genome_stats/filter_sites_cov_awk.sh
+++ genome_stats/filter_sites_cov_awk.sh
@@ -17,10 +17,9 @@ SCRIPT=~/het_grand/genome_stats/filter_sites_cov.R
 
 
 zcat $COV | \
-awk '	{OFS="\t"}
-		{s=$2-1}
-		{if(3 <  $3 && 3 < $4 && 5 < $5 && 5 < $6 && 5 < $7 && 3 < $8 && 5 < $9 && 5 < $10 && 3 < $11 && 5 < $12 && 3 < $13 && 3 < $14 && 3 < $15){print 1,$s,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15}}' | \
+awk '	{OFS="\t"}{s=$2-1}{if(3 <  $3 && 3 < $4 && 5 < $5 && 5 < $6 && 5 < $7 && 3 < $8 && 5 < $9 && 5 < $10 && 3 < $11 && 5 < $12 && 3 < $13 && 3 < $14 && 3 < $15){print $1,s,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15}}' | \
 $BED intersect -v -a stdin -b $HICOV | \
 $BED map -a $WIN -b stdin -c 3 -o count | \
-$BGZ -c  < popsites.1kb_win.bed.gz
+$BGZ -c  > popsites.1kb_win.bed.gz
+
 
