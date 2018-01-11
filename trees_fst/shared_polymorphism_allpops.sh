b55d4426dc080a8140c0fa805fd6f44078db8c79
Noah Reid <noah.reid@gmail.com>
2018-01-11 10:58:45 -0500

    sharedpp

--- trees_fst/shared_polymorphism_allpops.sh
+++ trees_fst/shared_polymorphism_allpops.sh
@@ -5,8 +5,15 @@ SCRIPT=~/het_grand/trees_fst/shared_polymorphism_allpops.R
 
 TAB=~/bin/htslib/tabix
 BGZ=~/bin/htslib/bgzip
+VAP=~/bin/vcflib/bin/vcfallelicprimitives
+
 VCF=/scratch/nmr15102/variants/hetgrand.hap.vcf.gz
 OUTDIR=/scratch/nmr15102/variants/shared_polymorphism_scaffold
 OUTFILE=$1.fst.gz
 
-$TAB $VCF $1 | Rscript $SCRIPT | $BGZ -c | cat >$OUTDIR/$OUTFILE
+$TAB $VCF $1 | \
+$VAP | \
+sed 's/:\.:\.:\.:\.:\.:\.:\.//g' | \
+Rscript $SCRIPT | \
+$BGZ -c | \
+cat >$OUTDIR/$OUTFILE
