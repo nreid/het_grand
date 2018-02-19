982a5f275899ca98b422e42a48108103b9f3ca5f
Noah Reid <noah.reid@gmail.com>
2018-02-19 15:36:35 -0500

    blah

---
+++ genome_stats/bamstats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# "module" alone was an unrecognized command. very confusing, the parallel slurm script runs this before "module" though. so I'm trying it. 
+source /etc/profile.d/modules.sh 
+
+module load samtools/1.3
+
+for file in $(cat bams.list)
+do samtools stats $file | gzip -c >$file.stats
+done
