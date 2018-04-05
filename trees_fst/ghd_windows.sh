9cf5de0e52fbd46339929386703d5ecfe98c1490
Noah Reid <noah.reid@gmail.com>
2018-04-05 19:05:49 -0400

    blah

--- trees_fst/ghd_windows.sh
+++ trees_fst/ghd_windows.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# "module" alone was an unrecognized command. very confusing, the parallel slurm script runs this before "module" though. so I'm trying it. 
 source /etc/profile.d/modules.sh 
-
-source ~/bin/parallel-slurm/parallel-slurm-setup.sh
 
 module load r/3.3.3
 
@@ -11,9 +8,5 @@ TAB=~/bin/htslib/tabix
 
 VCF=/scratch/nmr15102/variants/hetgrand.hap.vcf.gz
 
-for num in {1..18873}
-do win=$(sed -n $(echo $num)p /home/nmr15102/fhet_genome/100kb_win.bed)
-reg=$(echo $win | sed 's/ /:/' | sed 's/ /-/')
-tre=$(echo $win | sed 's/ /	/g')
-$TAB $VCF $reg | Rscript ~/het_grand/trees_fst/ghd_stream.R | sed "s/^/$tre	/"
-done
+reg=$(echo $1 | sed 's/:/	/' | sed 's/-/	/')
+$TAB $VCF $1 | Rscript ~/het_grand/trees_fst/ghd_stream.R | sed "s/^/$reg	/" >/scratch/nmr15102/intdist/singles/$1.ghd.out
