5d8616f34aa24c4345f5450a6a008250360595a4
Noah Reid <noah.reid@gmail.com>
2018-05-16 11:16:27 -0400

    blah

--- genome_stats/find_grandis_sexchr.sh
+++ genome_stats/find_grandis_sexchr.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-SCRIPT=~/het_grand/genome_stats/find_grandis_sexchr_lrat.R
+SCRIPT=~/het_grand/genome_stats/find_grandis_sexchr.R
 
 TAB=~/bin/htslib/tabix
 BGZ=~/bin/htslib/bgzip
 VCF=/scratch/nmr15102/variants/hetgrand.hap.vcf.gz
-OUTDIR=/scratch/nmr15102/genome_stats/grandsex/scafsex_lrat
+OUTDIR=/scratch/nmr15102/genome_stats/grandsex/scafsex
 OUTFILE=$1.chisq.gz
 
 $TAB $VCF $1 | Rscript $SCRIPT | $BGZ -c | cat >$OUTDIR/$OUTFILE
