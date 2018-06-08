b90196d25811f837445af94f23287688a82c26a1
Noah Reid <noah.reid@gmail.com>
2018-06-08 10:02:16 -0400

    blah

--- rna_vars/rnapopgenvars_region.sh
+++ rna_vars/rnapopgenvars_region.sh
@@ -10,10 +10,11 @@ FB=~/bin/freebayes/bin/freebayes
 VCFI=~/bin/vcflib/bin/vcfintersect
 GEN=/home/nmr15102/fhet_genome/GCF_000826765.1_Fundulus_heteroclitus-3.0.2_genomic.fasta
 BED=/scratch/nmr15102/tolerance_rnaseq/callregions_rna_union.bed
+BAMLIST=/scratch/nmr15102/variants/meta/popgenrnabams.list
 
 $FB -k -0 --min-coverage 750 --max-coverage 300000 \
 -f $GEN \
--L $DIR/alignments/meta/bams.list \
+-L $BAMLIST \
 -r $scaf | \
 $VCFI -b $BED | \
 awk '$6 > 30' >$OUTDIR/$scaf.vcf
