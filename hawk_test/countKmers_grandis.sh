1fcd739d53ced87e4b1e05306a7fab4c072de5c3
Noah Reid <noah.reid@gmail.com>
2019-02-27 13:08:52 -0500

    blah

--- hawk_test/countKmers_grandis.sh
+++ hawk_test/countKmers_grandis.sh
@@ -38,6 +38,7 @@ do
 	-s 2G \
 	<(samtools view -bhF 256 $file | $BED bamtofastq -i /dev/stdin -fq /dev/stdout )
 
+	# merge output files if there are > 1, then move file to another directory and rename
 	COUNT=$(ls $outdir/${OUTPREFIX}_kmers/tmp* | wc -l)
 
 	if [ $COUNT -eq 1 ]
@@ -46,31 +47,42 @@ do
 	else
 		${jellyfishDir}/jellyfish merge -o $outdir/${OUTPREFIX}_kmers_jellyfish $outdir/${OUTPREFIX}_kmers/tmp*
 	fi
+
 	rm -rf $outdir/${OUTPREFIX}_kmers
 	
 	COUNT=$(ls $outdir/${OUTPREFIX}_kmers_jellyfish | wc -l)
 
+	# assuming there is only one output file per input file...
 	if [ $COUNT -eq 1 ]
 	then
 
+		# run jellyfish histo
 		${jellyfishDir}/jellyfish histo -f -o $outdir/${OUTPREFIX}.kmers.hist.csv -t ${CORES} $outdir/${OUTPREFIX}_kmers_jellyfish
+		# reformat the hist file, only print cols 1 & 2 (dunno if there are more)
 		awk '{print $2"\t"$1}' $outdir/${OUTPREFIX}.kmers.hist.csv > $outdir/${OUTPREFIX}_tmp
+		# rename
 		mv $outdir/${OUTPREFIX}_tmp $outdir/${OUTPREFIX}.kmers.hist.csv
-
+		# count total number of kmers, write to a 
+			# the script used here must be modified, it excludes kmers that only appear once
+			# THIS FILE CONTAINS INFO FOR ALL SAMPLES AND CAN'T BE PARALLELIZED
 		awk -f ${hawkDir}/countTotalKmer.awk $outdir/${OUTPREFIX}.kmers.hist.csv >> ${outdir}/total_kmer_counts.txt
 
 		# don't use this cutoff. edited below. 
 		CUTOFF=1 
 		echo $CUTOFF > $outdir/${OUTPREFIX}_cutoff.csv
 
-
+		# run jellyfish dump, which outputs each kmer and its count, I think
 		${jellyfishDir}/jellyfish dump -c -L 1 $outdir/${OUTPREFIX}_kmers_jellyfish > $outdir/${OUTPREFIX}_kmers.txt 
+		# sort the kmer count file
 		${sortDir}/sort --parallel=${CORES} -n -k 1 $outdir/${OUTPREFIX}_kmers.txt > $outdir/${OUTPREFIX}_kmers_sorted.txt
-	
+		
+		# remove intermediate files	
 		rm $outdir/${OUTPREFIX}_kmers_jellyfish	
 		rm $outdir/${OUTPREFIX}_kmers.txt		
-			
-		echo "$outdir/${OUTPREFIX}_kmers_sorted.txt" >> ${dir}/sorted_files.txt
+		
+		# write sorted filename to a file
+			# THIS FILE CONTAINS INFO FOR ALL SAMPLES AND CAN'T BE PARALLELIZED
+		echo "$outdir/${OUTPREFIX}_kmers_sorted.txt" >> ${outdir}/sorted_files.txt
 		
 	fi
 
