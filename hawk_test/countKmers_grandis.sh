f9114afe090080883a0c92c2b2f55a29f8b1e926
Noah Reid <noah.reid@gmail.com>
2019-02-28 14:30:37 -0500

    blah

--- hawk_test/countKmers_grandis.sh
+++ hawk_test/countKmers_grandis.sh
@@ -4,6 +4,12 @@
 CORES=24		
 KMERSIZE=31
 
+# INSTEAD OF A LOOP OVER ALL FILES
+	# it would be easy to parallelize this
+	# push the lines that append results from all files
+	# to another script and run that when the k-mer
+	# counting is finished.
+
 # load samtools
 module load samtools/1.3
 
@@ -30,7 +36,7 @@ do
 	
 	mkdir $outdir/${OUTPREFIX}_kmers
 
-	# run jellyfish
+	# run jellyfish, converting bam to fastq
 	${jellyfishDir}/jellyfish count -C \
 	-o $outdir/${OUTPREFIX}_kmers/tmp \
 	-m ${KMERSIZE} \
@@ -57,22 +63,35 @@ do
 	then
 
 		# run jellyfish histo
-		${jellyfishDir}/jellyfish histo -f -o $outdir/${OUTPREFIX}.kmers.hist.csv -t ${CORES} $outdir/${OUTPREFIX}_kmers_jellyfish
+			# max high count is 10k at default
+			# this means # kmers in histogram is not 
+			# the same as the # you get by summing 
+			# the counts in the sorted.txt files
+		${jellyfishDir}/jellyfish histo \
+		-f \
+		-o $outdir/${OUTPREFIX}.kmers.hist.csv \
+		-t ${CORES} $outdir/${OUTPREFIX}_kmers_jellyfish
+
 		# reformat the hist file, only print cols 1 & 2 (dunno if there are more)
 		awk '{print $2"\t"$1}' $outdir/${OUTPREFIX}.kmers.hist.csv > $outdir/${OUTPREFIX}_tmp
+
 		# rename
 		mv $outdir/${OUTPREFIX}_tmp $outdir/${OUTPREFIX}.kmers.hist.csv
+
 		# count total number of kmers, write to a 
 			# the script used here must be modified, it excludes kmers that only appear once
 			# THIS FILE CONTAINS INFO FOR ALL SAMPLES AND CAN'T BE PARALLELIZED
 		awk -f ${hawkDir}/countTotalKmer.awk $outdir/${OUTPREFIX}.kmers.hist.csv >> ${outdir}/total_kmer_counts.txt
 
-		# don't use this cutoff. edited below. 
+		# don't use this kmer frequency cutoff. edited below. 
 		CUTOFF=1 
 		echo $CUTOFF > $outdir/${OUTPREFIX}_cutoff.csv
 
 		# run jellyfish dump, which outputs each kmer and its count, I think
-		${jellyfishDir}/jellyfish dump -c -L 1 $outdir/${OUTPREFIX}_kmers_jellyfish > $outdir/${OUTPREFIX}_kmers.txt 
+		${jellyfishDir}/jellyfish dump \
+		-c -L 1 \
+		$outdir/${OUTPREFIX}_kmers_jellyfish > $outdir/${OUTPREFIX}_kmers.txt 
+
 		# sort the kmer count file
 		${sortDir}/sort --parallel=${CORES} -n -k 1 $outdir/${OUTPREFIX}_kmers.txt > $outdir/${OUTPREFIX}_kmers_sorted.txt
 		
