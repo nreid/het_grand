e1428547946e6afe7906751162e6b08f65f5d67d
Noah Reid <noah.reid@gmail.com>
2019-02-28 20:12:03 -0500

    blah

--- hawk_test/runhawk_grandis.sh
+++ hawk_test/runhawk_grandis.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# needed to recompile eigenstrat, 
+# and recompiled version won't run without these modules loaded
+
 module load blas/openblas-0.2.18
 module load gsl/2.4
 module load lapack/3.7.1
-
+module load r/3.4.1
 
 hawkDir=~/bin/HAWK
 eigenstratDir=~/bin/HAWK/supplements/EIG6.0.1-Hawk
